b27d9089134cc3d044ba5835bea5fd68c3a22fc4
Xicheng Guo <xicheng_guo@wistron.com>
2023-12-27 16:23:53 +0800

    update docs

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,6 +4,9 @@ import { registerComponentsPlugin } from '@vuepress/plugin-register-components';
 import { getDirname, path } from '@vuepress/utils'
 const __dirname = getDirname(import.meta.url)
 export default defineUserConfig({
+  head: [
+    ['script', { src: '/js/snowfall.js', async: true }],
+  ],
   base: "/",
 
   lang: "zh-CN",
